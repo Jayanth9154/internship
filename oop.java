d2705f593d3dc08d34b93dbcd5d00159efdb5ef2
m.jayanth <m.jayanth431@outlook.com>
2025-08-19 12:24:15 +0530

    this is the object

---
+++ oop.java
@@ -0,0 +1,15 @@
+
+class Car {
+    void drive() {
+        System.out.println("The car is driving...");
+    }
+}
+
+
+public class oop {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.drive();
+    }
+}
