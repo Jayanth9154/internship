ac8a053007d47524b4b8a3a2c2be95dc8eae0e8b
m.jayanth <m.jayanth431@outlook.com>
2025-10-07 20:50:08 +0530

    Added presentation and updated UserRepository

--- project/backend/src/main/java/com/neurofleetx/repository/UserRepository.java
+++ project/backend/src/main/java/com/neurofleetx/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.neurofleetx.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
